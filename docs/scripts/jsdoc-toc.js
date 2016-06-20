efd927b7c034c6f904640353205d6a75c8f636b7
Alex Bates <16batesa@gmail.com>
2016-06-20 17:02:03 +0100

    Compile last commit

--- docs/scripts/jsdoc-toc.js
+++ docs/scripts/jsdoc-toc.js
@@ -6,7 +6,7 @@
     treeNode.tree({
         autoEscape: false,
         closedIcon: '&#x21e2;',
-        data: [{"label":"<a href=\"Sprinting.html\">Sprinting</a>","id":"Sprinting","children":[{"label":"<a href=\"Sprinting.Circle.html\">Circle</a>","id":"Sprinting.Circle","children":[]},{"label":"<a href=\"Sprinting.Img.html\">Img</a>","id":"Sprinting.Img","children":[]},{"label":"<a href=\"Sprinting.Rectangle.html\">Rectangle</a>","id":"Sprinting.Rectangle","children":[]},{"label":"<a href=\"Sprinting.Shape.html\">Shape</a>","id":"Sprinting.Shape","children":[]},{"label":"<a href=\"Sprinting.Thing.html\">Thing</a>","id":"Sprinting.Thing","children":[]},{"label":"<a href=\"Sprinting.World.html\">World</a>","id":"Sprinting.World","children":[]},{"label":"<a href=\"Sprinting.pointer.html\">pointer</a>","id":"Sprinting.pointer","children":[]}]}],
+        data: [{"label":"<a href=\"Sprinting.html\">Sprinting</a>","id":"Sprinting","children":[{"label":"<a href=\"Sprinting.Circle.html\">Circle</a>","id":"Sprinting.Circle","children":[]},{"label":"<a href=\"Sprinting.Img.html\">Img</a>","id":"Sprinting.Img","children":[]},{"label":"<a href=\"Sprinting.Rectangle.html\">Rectangle</a>","id":"Sprinting.Rectangle","children":[]},{"label":"<a href=\"Sprinting.Shape.html\">Shape</a>","id":"Sprinting.Shape","children":[]},{"label":"<a href=\"Sprinting.Thing.html\">Thing</a>","id":"Sprinting.Thing","children":[]},{"label":"<a href=\"Sprinting.World.html\">World</a>","id":"Sprinting.World","children":[]}]}],
         openedIcon: ' &#x21e3;',
         saveState: true,
         useContextMenu: false
